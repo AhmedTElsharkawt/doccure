d934451eb40f2a283bc81f381f1ceb4b71389566
Ahmed Elsharkawy <137622571+AhmedTElsharkawt@users.noreply.github.com>
2024-11-15 15:12:56 +0200

    Add files via upload

--- doccuresharkawy.sql
+++ doccuresharkawy.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 03, 2024 at 07:19 PM
+-- Generation Time: Nov 15, 2024 at 02:12 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,35 +24,35 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `admins`
+--
+
+CREATE TABLE `admins` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL DEFAULT 'https://img.freepik.com/premium-vector/silver-membership-icon-default-avatar-profile-icon-membership-icon-social-media-user-image-vector-illustration_561158-4195.jpg?semt=ais_hybrid',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `appointments`
 --
 
 CREATE TABLE `appointments` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `date` date DEFAULT NULL,
-  `time` time NOT NULL,
+  `date` varchar(255) NOT NULL,
+  `time` varchar(255) NOT NULL,
   `user_id` bigint(20) UNSIGNED NOT NULL,
   `doctor_id` bigint(20) UNSIGNED NOT NULL,
   `status` varchar(255) NOT NULL DEFAULT 'pending',
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `appointments`
---
-
-INSERT INTO `appointments` (`id`, `date`, `time`, `user_id`, `doctor_id`, `status`, `created_at`, `updated_at`) VALUES
-(12, '2024-10-30', '18:57:00', 1, 1, 'cancelled', '2024-10-30 12:57:03', '2024-10-30 12:57:03'),
-(13, '2024-10-30', '19:05:00', 1, 1, 'pending', '2024-10-30 13:05:34', '2024-10-30 13:05:34'),
-(14, '2024-11-09', '19:09:00', 1, 2, 'accepted', '2024-10-31 13:09:29', '2024-10-31 13:09:29'),
-(15, '2024-11-07', '20:55:00', 1, 1, 'accepted', '2024-10-31 14:55:17', '2024-10-31 14:55:17'),
-(17, '2024-11-06', '13:00:00', 1, 2, 'pending', '2024-10-31 15:01:31', '2024-10-31 15:01:31'),
-(19, '2024-11-03', '10:45:00', 1, 2, 'pending', '2024-11-01 11:43:05', '2024-11-01 11:43:05'),
-(20, '2024-11-02', '16:07:00', 1, 3, 'pending', '2024-11-01 12:07:32', '2024-11-01 15:33:54'),
-(21, '2024-11-12', '20:07:00', 1, 3, 'accepted', '2024-11-01 12:07:57', '2024-11-01 15:32:37'),
-(22, '2024-11-19', '16:19:00', 1, 3, 'cancelled', '2024-11-01 12:19:04', '2024-11-01 16:00:44'),
-(23, '2024-11-15', '04:21:00', 1, 3, 'pending', '2024-11-01 12:19:17', '2024-11-01 14:49:58');
 
 -- --------------------------------------------------------
 
@@ -70,18 +70,19 @@ CREATE TABLE `doctors` (
   `bio` text DEFAULT NULL,
   `price` varchar(255) DEFAULT NULL,
   `image` varchar(255) NOT NULL DEFAULT 'https://img.freepik.com/premium-vector/silver-membership-icon-default-avatar-profile-icon-membership-icon-social-media-user-image-vector-illustration_561158-4195.jpg?semt=ais_hybrid',
+  `dates` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (json_valid(`dates`)),
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `status` enum('pending','confirmed') NOT NULL DEFAULT 'pending'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `doctors`
 --
 
-INSERT INTO `doctors` (`id`, `name`, `email`, `password`, `mobile`, `country`, `bio`, `price`, `image`, `created_at`, `updated_at`) VALUES
-(1, 'Darren Elder', 'doc1@g.com', '$2y$10$XDv2tpMbBhD09zHaR6eyceEVQ10ZDz60VdY2xrB0P9QJqxPbmG7NS', '01003238467', 'Florida, USA', 'MDS - Periodontology and Oral Implantology, BDS', '$300', 'images/2024-10-30-6721bf820c806.jpg', '2024-10-29 15:51:31', '2024-11-03 15:42:12'),
-(2, 'Ruby Perrin', 'doc2@g.com', '$2y$10$4W.Hnzan.3Vd4N1jmcL8N.e.bTTcAgpc7EcEVWhLcJ5R4O58e5MTO', '01223139813', 'Florida, USA', 'MDS - Periodontology and Oral Implantology, BDS', '$150', 'images/2024-10-30-6721e379483fb.jpg', '2024-10-30 04:35:11', '2024-10-30 04:42:49'),
-(3, 'Marvin Campbell', 'doc3@g.com', '$2y$10$.nRzs5USKtAUPVajSCbTsOiz9VNrMNkLl50jxu.0.JnfSKoYkgxu6', '01003238467', 'Michigan, USA', 'MBBS, MD - Ophthalmology, DNB - Ophthalmology', '$85', 'images/2024-10-31-6723cd53c928f.jpg', '2024-10-31 15:30:31', '2024-10-31 15:32:51');
+INSERT INTO `doctors` (`id`, `name`, `email`, `password`, `mobile`, `country`, `bio`, `price`, `image`, `dates`, `created_at`, `updated_at`, `status`) VALUES
+(1, 'Derek Buckner', 'doc1@g.com', '$2y$10$p/Tf0FFYlBQRpWQK86XRRO5KuWa3CkpkxIIJC/pGHeWIUy6JTiIry', '165468156984', 'Odit sit eum error', 'Sit Nam quaerat ut', '42', 'https://img.freepik.com/premium-vector/silver-membership-icon-default-avatar-profile-icon-membership-icon-social-media-user-image-vector-illustration_561158-4195.jpg?semt=ais_hybrid', '[{\"day\":\"Monday\",\"start_time\":\"01:00\",\"end_time\":\"06:00\"},{\"day\":\"Tuesday\",\"start_time\":\"01:00\",\"end_time\":\"06:00\"}]', '2024-11-11 17:23:42', '2024-11-15 09:21:44', 'pending'),
+(2, 'Gray Hickman', 'sotyrivep@mailinator.com', '$2y$10$WG4wg67yKotvTkA870TjO.nd3jUdu16gLk8WZ1h1RG9mJOiSWA8pq', NULL, NULL, NULL, NULL, 'https://img.freepik.com/premium-vector/silver-membership-icon-default-avatar-profile-icon-membership-icon-social-media-user-image-vector-illustration_561158-4195.jpg?semt=ais_hybrid', NULL, '2024-11-13 16:48:51', '2024-11-15 09:21:50', 'pending');
 
 -- --------------------------------------------------------
 
@@ -122,7 +123,12 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (4, '2019_08_19_000000_create_failed_jobs_table', 1),
 (5, '2019_12_14_000001_create_personal_access_tokens_table', 1),
 (6, '2024_10_29_110305_create_doctors_table', 1),
-(7, '2024_10_29_114910_create_appointments_table', 1);
+(7, '2024_10_29_114910_create_appointments_table', 1),
+(8, '2024_11_11_184832_add_deleted_at_to_appointments', 2),
+(9, '2024_11_12_183453_add_email_verifyed_at_to_users', 3),
+(10, '2024_11_13_185811_add_status_to_doctors', 4),
+(11, '2024_11_13_194432_add_admin_to_users', 5),
+(12, '2024_11_15_091817_create_admins_table', 6);
 
 -- --------------------------------------------------------
 
@@ -185,20 +191,29 @@ CREATE TABLE `users` (
   `image` varchar(255) NOT NULL DEFAULT 'https://img.freepik.com/premium-vector/silver-membership-icon-default-avatar-profile-icon-membership-icon-social-media-user-image-vector-illustration_561158-4195.jpg?semt=ais_hybrid',
   `remember_token` varchar(100) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `admin` enum('0','1') NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `password`, `date_of_birth`, `blood_group`, `mobile`, `country`, `image`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Richard Wilson', 'user1@g.com', '$2y$10$ERADTcULtPVmTDHxsd4UF.V95nkE0FOCIF9NfNGz6m8SUeICCTvtK', '1992-06-29', 'A-', '01223139813', 'New york, USA', 'images/2024-10-30-6721f3f363609.jpg', NULL, NULL, '2024-11-02 19:02:32'),
-(2, 'Travis Trimble', 'user2@g.com', '$2y$10$R.bYscGURhW6r/EJpPycsejZzXapT7g8kBAopXQj9RJNPZpf7LJP.', NULL, NULL, NULL, NULL, 'https://img.freepik.com/premium-vector/silver-membership-icon-default-avatar-profile-icon-membership-icon-social-media-user-image-vector-illustration_561158-4195.jpg?semt=ais_hybrid', NULL, '2024-10-30 03:41:04', '2024-10-30 03:41:04');
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `date_of_birth`, `blood_group`, `mobile`, `country`, `image`, `remember_token`, `created_at`, `updated_at`, `email_verified_at`, `admin`) VALUES
+(12, 'Admin1', 'admin@g.com', '$2y$10$3ZlIjQ6XNtSdJMyJ0jIrUOcDmftOou/Twdzf232v0Y3H1DvidHQ5q', '1996-06-11', 'A-', NULL, NULL, 'https://img.freepik.com/premium-vector/silver-membership-icon-default-avatar-profile-icon-membership-icon-social-media-user-image-vector-illustration_561158-4195.jpg?semt=ais_hybrid', NULL, '2024-11-13 17:46:57', '2024-11-13 19:53:46', '2024-11-13 17:47:30', '1'),
+(13, 'ahmed', 'user1@g.com', '$2y$10$nPb58sJc1vUczi9SmjXJfuUrrNwg/CTx/BC/i0xVYgI6.v6LJ/uLm', NULL, NULL, NULL, NULL, 'https://img.freepik.com/premium-vector/silver-membership-icon-default-avatar-profile-icon-membership-icon-social-media-user-image-vector-illustration_561158-4195.jpg?semt=ais_hybrid', NULL, '2024-11-14 18:18:37', '2024-11-14 18:19:26', '2024-11-14 18:19:26', '0');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `admins_email_unique` (`email`);
 
 --
 -- Indexes for table `appointments`
@@ -260,16 +275,22 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `appointments`
 --
 ALTER TABLE `appointments`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `doctors`
 --
 ALTER TABLE `doctors`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `failed_jobs`
@@ -281,7 +302,7 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `personal_access_tokens`
@@ -293,7 +314,7 @@ ALTER TABLE `personal_access_tokens`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- Constraints for dumped tables
